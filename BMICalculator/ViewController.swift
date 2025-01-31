161400b85dbd57436831b26813db35f0eaa39d98
V4RM4 <vaisakhsvarma@gmail.com>
2025-01-31 12:30:58 -0500

    Initial Commit

---
+++ BMICalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by Vaisakh Suresh on 2025-01-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
